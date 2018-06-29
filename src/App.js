fbe3d550f7ba45fbc7108ffa3957d5d186d9ff68
John Staudt <john.staudt@uicentric.com>
2018-06-29 15:36:36 -0500

    Add state property and click handler.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    people: [
+      { name: 'Max', age: 12 },
+      { name: 'Mix', age: 22 },
+      { name: 'Mux', age: 32 },
+    ]
+  }
+
+  switchNameHandler = (newName) =>
+  {
+    // console.log('yoooooo');
+    //DON'T DO THIS: this.state.people[0].name = '';
+    this.setState({
+      people: [
+        { name: newName, age: 12 },
+        { name: 'Mix', age: 22 },
+        { name: 'Mux', age: 32 },
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Yo I'm react app.</h1>
+        <p>works</p>
+        <button onClick={this.switchNameHandler.bind(this, 'Mookie')}>switch name</button>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age}/>
+        <Person
+          name={this.state.people[1].name}
+          age={this.state.people[1].age}
+          click={this.switchNameHandler.bind(this, 'Brick')}/>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}/>
       </div>
     );
+    // return React.createElement('div', null, 'h1', 'ey, I\'m');
   }
 }
 
