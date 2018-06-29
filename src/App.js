05e238240eb3ad728d30624d4b8a177b72d6a246
John Staudt <john.staudt@uicentric.com>
2018-06-29 16:39:30 -0500

    Add jsconfig and styles.

--- src/App.js
+++ src/App.js
@@ -24,17 +24,38 @@ class App extends Component {
     });
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      people: [
+        { name: 'Max', age: 12 },
+        { name: event.target.value, age: 22 },
+        { name: 'Mux', age: 32 },
+      ]
+    });
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px'
+    }
+
     return (
       <div className="App">
         <h1>Yo I'm react app.</h1>
         <p>works</p>
-        <button onClick={this.switchNameHandler.bind(this, 'Mookie')}>switch name</button>
+        <button
+          style={style}
+          onClick={this.switchNameHandler.bind(this, 'Mookie')}>switch name
+        </button>
         <Person name={this.state.people[0].name} age={this.state.people[0].age}/>
         <Person
           name={this.state.people[1].name}
           age={this.state.people[1].age}
-          click={this.switchNameHandler.bind(this, 'Brick')}/>
+          click={this.switchNameHandler.bind(this, 'Brick')}
+          changed={this.nameChangedHandler}/>
         <Person name={this.state.people[2].name} age={this.state.people[2].age}/>
       </div>
     );
